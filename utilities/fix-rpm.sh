f6baed9d4e546db4346e306a0c125dcb1763bfe2
Joel Peck <peckjd@gmail.com>
2021-10-03 15:16:29 -0400

    Added links to relevant RHEL KB solutions

--- utilities/fix-rpm.sh
+++ utilities/fix-rpm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# https://access.redhat.com/solutions/6903
+# https://access.redhat.com/solutions/3955251
+
 sudo rm -f /var/lib/rpm/__db.*
 sudo rpm -vv --rebuilddb
 sudo /usr/lib/rpm/rpmdb_verify /var/lib/rpm/Packages
